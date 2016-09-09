6c990ee9f1544c9b90739d94ff633a66d46cc408
sl_mac <sl@SL-Mac.local>
2016-09-09 13:10:23 +0530

    Implemented PopUp for help window for the users

--- lite/app/pages/pop-up/pop-up.ts
+++ lite/app/pages/pop-up/pop-up.ts
@@ -1,18 +1,26 @@
 import { Component } from '@angular/core';
-import {NavController, NavParams} from 'ionic-angular';
-
-
+import { Alert, NavController, NavParams} from 'ionic-angular';
 
 @Component({
   templateUrl: 'build/pages/pop-up/pop-up.html',
 })
 
-
 export class PopUpPage {
-  
-  constructor(private navCtrl: NavController, navParams: NavParams) {
-    
-    
-  }
-  
+    static get parameters() {
+        return [[NavController]];
+    }
+    constructor(private nav: NavController) {
+        this.nav = nav;
+    }
+
+    showAlert() {
+        let alert = Alert.create({
+            title: 'Help Time!',
+            subTitle: 'Mindfulness is here for you and your soul. We are intended to stablish your connection to All Mighty.',
+            buttons: ['Cancle']
+        });
+        this.nav.present(alert);
+
+    }
 }
+
