d5f5c854684a3202de2b18a675f7cc7d34c7c403
Sanjay Yadav <sanjay.yadav@subtlelabs.com>
2016-10-14 11:16:39 +0530

    All html are being clicked and opening now in new html view with specific URL

---
+++ lite/app/pages/view/get_page.service.ts
@@ -0,0 +1,29 @@
+import { NavController, NavParams } from 'ionic-angular';
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import { Observable } from "rxjs/Rx";
+import { ViewPage } from '../view/view';
+
+@Injectable()
+export class GetPageService {
+
+    private url;
+    private retryCount = 2;
+
+    constructor(private _http: Http, navParams: NavParams) {
+      this.url = navParams.get('url');
+    }
+
+    getPage(){
+      return this._http.get(this.url)
+        .retry(this.retryCount)
+        .map((res) => {
+            return res.url;
+        })
+        .catch((err)=>{
+        let errMsg = (err.error) ? err.errmsg : err;
+            console.error(errMsg);
+            return Observable.throw(errMsg);
+        })
+    }
+}
--- lite/app/pages/view/view.ts
+++ lite/app/pages/view/view.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
+import {DomSanitizationService} from '@angular/platform-browser';
 import { NavController, NavParams } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import {GetPageService} from "./get_page.service";
+import { Http } from '@angular/http';
 
 
 @Component({
   templateUrl: 'build/pages/view/view.html',
+  providers: [GetPageService]
 })
+
 export class ViewPage {
 
-	url: any;
+  //url: any;
+  page: any;
 
-  constructor(private nav: NavController, navParams: NavParams) {
-    this.url= navParams.get('url'); 
+  constructor(private _gps: GetPageService, private sanitizer: DomSanitizationService, private nav: NavController, navParams: NavParams ) {
+    
+    // this.url = navParams.get('url'); 
+    // console.log(this.url);
   }
 
+  ngOnInit() {
+    this._gps.getPage().subscribe(
+        (data)=>{
+          this.page = this.sanitizer.bypassSecurityTrustResourceUrl(data);
+          //console.log(this.page);
+        }
+    )
+  }
 }
