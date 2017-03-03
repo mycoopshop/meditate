aa5d8614ed61fcd09c30b5c8d7a230f7f23ef01c
Sanjay Yadav <sanjay.yadav@subtlelabs.com>
2017-03-03 12:39:44 +0530

    Implemented new icon, splashScreen, changed view page, implemented audio player etc

--- lite/app/app.ts
+++ lite/app/app.ts
@@ -27,28 +27,33 @@ class MyApp {
     }, 1000);
 
     this.pages = [
-      { title: 'WELCOME',                    type: 'main menu',  component: VideoPage },
-      { title: 'Introduction',               type: 'sub menu' ,  component: 'assets/read/ch_5_the_art_of_allowing/the_art_of_allowing.html' },
-      { title: 'How to use this app',        type: 'sub menu' ,  component: 'assets/challenges/eight_day_micro_course/module_eight_turning_towards.html' },
-      { title: 'ABOUT',                      type: 'main menu',  component: AudioPage     },
-      { title: 'Mindfulness in a few words', type: 'sub menu' ,  component: 'assets/read/ch_5_the_art_of_allowing/the_observer_within_part_two.html' },
-      { title: 'Psychology influences',      type: 'sub menu' ,  component: 'assets/read/ch_4_patterns_of_behaviour/2_inner_resistance/in_or_out_of_control.html' },
-      { title: 'About Reuben',               type: 'sub menu' ,  component: 'assets/read/ch_3_mind_and_emotions/3_feelings_as_feelings/normal_human_emotions.html' },
-      { title: 'About Mindful Creation',     type: 'sub menu' ,  component: 'assets/read/ch_3_mind_and_emotions/2_thoughts_as_thoughts/associations_part_two.html' },
-      { title: 'Disclaimer',                 type: 'sub menu' ,  component: 'assets/read/ch_2_introduction_to_mindfulness/overcoming_boredom_part_one.html' },
-      { title: 'Terms and Conditions',       type: 'sub menu' ,  component: 'assets/challenges/eight_day_micro_course/end_of_course.html' },
-      { title: 'SETTINGS',                   type: 'main menu',  component: PopUpPage      },
-      { title: 'Random insights',            type: 'sub menu' ,  component: 'assets/explore/i_like_them_short/tips_and_tools/stepping_back.html' },
-      { title: 'Random meditation tips',     type: 'sub menu' ,  component: 'assets/explore/i_like_them_short/guided_mindfulness/getting_with_the_flow_seven_mins.html' },
-      { title: 'MY VIDEO',                   type: 'main menu',  component: VideoPage },
-      { title: 'MORE',                       type: 'main menu',  component: AudioPage },
-      { title: 'Tap me!',                    type: 'sub menu' ,  component: 'assets/explore/i_like_them_short/tips_and_tools/stepping_back.html' },
-      { title: 'Set reminder',               type: 'sub menu' ,  component: 'assets/explore/i_like_them_short/guided_mindfulness/getting_with_the_flow_seven_mins.html' },
-      { title: 'Share with friends',         type: 'sub menu' ,  component: 'assets/explore/i_like_them_short/experiential_exercises/stories_on_a_stream.html' },
-      { title: 'Sign up',                    type: 'sub menu' ,  component: 'assets/read/ch_2_introduction_to_mindfulness/meaningful_connection_part_one.html' },
-      { title: 'Our apps',                   type: 'sub menu' ,  component: 'assets/read/ch_2_introduction_to_mindfulness/overcoming_boredom_part_one.html' },
-      { title: 'News',                       type: 'sub menu' ,  component: 'assets/read/ch_2_introduction_to_mindfulness/a_new_dimension_to_things.html' },
-      { title: 'Thank you',                  type: 'sub menu' ,  component: 'assets/challenges/eight_day_micro_course/module_four_emotions_and_feeling.html' }
+      { title: 'START HERE',                            type: 'main menu',  component: VideoPage },
+      { title: 'Welcome',                               type: 'sub menu' ,  component: 'assets/read/ch_5_the_art_of_allowing/the_art_of_allowing.html' },
+      { title: 'Why do this?',                          type: 'sub menu' ,  component: 'assets/read/ch_5_the_art_of_allowing/the_art_of_allowing.html' },
+      { title: 'How to use this app',                   type: 'sub menu' ,  component: 'assets/challenges/eight_day_micro_course/module_eight_turning_towards.html' },
+      { title: 'INTRO VIDEOS',                          type: 'main menu',  component: AudioPage     },
+      { title: 'Mindfulness is like a triangle',        type: 'sub menu' ,  component: 'assets/read/ch_5_the_art_of_allowing/the_observer_within_part_two.html' },
+      { title: 'BMW',                                   type: 'sub menu' ,  component: 'assets/read/ch_4_patterns_of_behaviour/2_inner_resistance/in_or_out_of_control.html' },
+      { title: 'How to practice mindfulness',           type: 'sub menu' ,  component: 'assets/read/ch_3_mind_and_emotions/3_feelings_as_feelings/normal_human_emotions.html' },
+      { title: "When your mind says “I can't do it!”" , type: 'sub menu' ,  component: 'assets/read/ch_3_mind_and_emotions/2_thoughts_as_thoughts/associations_part_two.html' },
+      { title: 'Thought are like rain on windscreen',   type: 'sub menu' ,  component: 'assets/read/ch_2_introduction_to_mindfulness/overcoming_boredom_part_one.html' },
+      { title: 'SETTINGS',                              type: 'main menu',  component: PopUpPage      },
+      { title: 'Random insights reminder',              type: 'sub menu' ,  component: 'assets/explore/i_like_them_short/tips_and_tools/stepping_back.html' },
+      { title: 'MY DOWNLOADS',                          type: 'main menu',  component: VideoPage },
+      { title: 'My video and audio downloads',          type: 'sub menu' ,  component: 'assets/explore/i_like_them_short/guided_mindfulness/getting_with_the_flow_seven_mins.html' },
+      { title: 'ABOUT ME',                              type: 'main menu',  component: AudioPage },
+      { title: 'About Reuben',                          type: 'sub menu' ,  component: 'assets/explore/i_like_them_short/tips_and_tools/stepping_back.html' },
+      { title: 'Psychology influences',                 type: 'sub menu' ,  component: 'assets/explore/i_like_them_short/tips_and_tools/stepping_back.html' },
+      { title: 'About Mindfulness Creation',            type: 'sub menu' ,  component: 'assets/explore/i_like_them_short/tips_and_tools/stepping_back.html' },
+      { title: 'MORE',                                     type: 'main menu',  component: AudioPage },
+      { title: 'Tap me!',                                  type: 'sub menu' ,  component: 'assets/explore/i_like_them_short/tips_and_tools/stepping_back.html' },
+      { title: 'Share with friends',                       type: 'sub menu' ,  component: 'assets/explore/i_like_them_short/experiential_exercises/stories_on_a_stream.html' },
+      { title: 'Sign up',                                  type: 'sub menu' ,  component: 'assets/explore/i_like_them_short/guided_mindfulness/getting_with_the_flow_seven_mins.html' },
+      { title: 'Our apps',                                 type: 'sub menu' ,  component: 'assets/read/ch_2_introduction_to_mindfulness/overcoming_boredom_part_one.html' },
+      { title: 'News',                                     type: 'sub menu' ,  component: 'assets/read/ch_2_introduction_to_mindfulness/a_new_dimension_to_things.html' },
+      { title: 'Thank you',                                type: 'sub menu' ,  component: 'assets/challenges/eight_day_micro_course/module_four_emotions_and_feeling.html' },
+      { title: 'Disclaimer',                               type: 'sub menu' ,  component: 'assets/read/ch_2_introduction_to_mindfulness/meaningful_connection_part_one.html' },
+      { title: 'Privacy policy and terms and conditions',  type: 'sub menu' ,  component: 'assets/read/ch_2_introduction_to_mindfulness/meaningful_connection_part_one.html' }
     ];
   }
 
@@ -56,17 +61,18 @@ class MyApp {
     this.platform.ready().then(() => {
       StatusBar.overlaysWebView(false);
       StatusBar.hide();
-      Splashscreen.hide();
+      // Splashscreen.hide();
+      this.hideSplashScreen();
     });
   }
 
-  // hideSplashScreen() {
-  //   if (Splashscreen) {
-  //     setTimeout(() => {
-  //       Splashscreen.hide();
-  //     }, 100);
-  //   }
-  // }
+  hideSplashScreen() {
+    if (Splashscreen) {
+      setTimeout(() => {
+        Splashscreen.hide();
+      }, 110);
+    }
+  }
 
   goToHome() {
     this.nav.insert(0, MenuPage,{},{direction: 'back', duration: 490});
